1d857bbff4dbd388acab2d421e633ba8467c0794
Cristina.Alekseeva <cristina.alekseeva@gmail.com>
2020-03-17 17:24:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Card,
+  CardContent,
+  IconButton,
+  Collapse
+} from "@material-ui/core";
+import clsx from "clsx";
+import SettingsIcon from "@material-ui/icons/Settings";
+import DiscreteSlider from "./components/DiscreteSlider";
+import Expansion from "./components/Expansion";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <Expansion />
+        <CardContent>
+          <p>
+            (искл. внутренние переводы, кредиты/займы, возвраты авансов и
+            ошибочных платежей)
+          </p>
+
+          <DiscreteSlider />
+        </CardContent>
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
