6b744890cad60ded76b7d4444dd2dada7c0bb98b
Cristina.Alekseeva <cristina.alekseeva@gmail.com>
2020-03-18 23:55:08 +0300

    Updates

---
+++ precache-manifest.6f739b1405909f890bddc4151a923bc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0029098764f128fdf5ebf8e2c2fc99c",
+    "url": "/karma/index.html"
+  },
+  {
+    "revision": "c64f4ff642f02daab93a",
+    "url": "/karma/static/css/2.9ef65634.chunk.css"
+  },
+  {
+    "revision": "8c6c1c8bbedf3a29e155",
+    "url": "/karma/static/css/main.edbb6bc7.chunk.css"
+  },
+  {
+    "revision": "c64f4ff642f02daab93a",
+    "url": "/karma/static/js/2.0552463c.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/karma/static/js/2.0552463c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6c1c8bbedf3a29e155",
+    "url": "/karma/static/js/main.d760b743.chunk.js"
+  },
+  {
+    "revision": "e05e4acbfe95e2414127",
+    "url": "/karma/static/js/runtime-main.8bd06e32.js"
+  }
+]);
